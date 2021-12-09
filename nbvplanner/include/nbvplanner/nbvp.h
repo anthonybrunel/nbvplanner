10d7ccc4f9a2f395dce6c2df449cba248911fcea
Anthony Brunel <anthony.brunel030@gmail.com>
2021-12-09 14:16:36 +0100

    update version of volumetric_mapping for ubuntu 20.04

--- nbvplanner/include/nbvplanner/nbvp.h
+++ nbvplanner/include/nbvplanner/nbvp.h
@@ -36,53 +36,53 @@
 
 namespace nbvInspection {
 
-    template<typename stateVec>
-    class nbvPlanner
+template<typename stateVec>
+class nbvPlanner
 {
 
-        ros::NodeHandle nh_;
-        ros::NodeHandle nh_private_;
+    ros::NodeHandle nh_;
+    ros::NodeHandle nh_private_;
 
-        std::shared_ptr<octomap::OcTree> ot_;
-        ros::Subscriber octomap_sub_;
+    std::shared_ptr<octomap::OcTree> ot_;
+    ros::Subscriber octomap_sub_;
 
-        ros::Subscriber posClient_;
-        ros::Subscriber odomClient_;
-        ros::Subscriber peerPosClient1_;
-        ros::Subscriber peerPosClient2_;
-        ros::Subscriber peerPosClient3_;
-        ros::Subscriber evadeClient_;
-        ros::Publisher evadePub_;
-        ros::ServiceServer plannerService_;
-        ros::Subscriber pointcloud_sub_;
-        ros::Subscriber pointcloud_sub_cam_up_;
-        ros::Subscriber pointcloud_sub_cam_down_;
-        Params params_;
-        mesh::StlMesh * mesh_;
-        //  volumetric_mapping::OctomapManager * manager_;
+    ros::Subscriber posClient_;
+    ros::Subscriber odomClient_;
+    ros::Subscriber peerPosClient1_;
+    ros::Subscriber peerPosClient2_;
+    ros::Subscriber peerPosClient3_;
+    ros::Subscriber evadeClient_;
+    ros::Publisher evadePub_;
+    ros::ServiceServer plannerService_;
+    ros::Subscriber pointcloud_sub_;
+    ros::Subscriber pointcloud_sub_cam_up_;
+    ros::Subscriber pointcloud_sub_cam_down_;
+    Params params_;
+    mesh::StlMesh * mesh_;
+    volumetric_mapping::OctomapManager * manager_;
 
-        bool ready_;
+    bool ready_;
 
-        public:
-        typedef std::vector<stateVec> vector_t;
-        TreeBase<stateVec> * tree_;
+public:
+    typedef std::vector<stateVec> vector_t;
+    TreeBase<stateVec> * tree_;
 
-        nbvPlanner(const ros::NodeHandle& nh, const ros::NodeHandle& nh_private);
-        ~nbvPlanner();
-        bool setParams();
+    nbvPlanner(const ros::NodeHandle& nh, const ros::NodeHandle& nh_private);
+    ~nbvPlanner();
+    bool setParams();
 
 
-        void octomapCallback(const octomap_msgs::Octomap& msg);
+    void octomapCallback(const octomap_msgs::Octomap& msg);
 
-        void posCallback(const geometry_msgs::PoseWithCovarianceStamped& pose);
-        void odomCallback(const nav_msgs::Odometry& pose);
-        bool plannerCallback(nbvplanner::nbvp_srv::Request& req, nbvplanner::nbvp_srv::Response& res);
-        void insertPointcloudWithTf(const sensor_msgs::PointCloud2::ConstPtr& pointcloud);
-        void insertPointcloudWithTfCamUp(const sensor_msgs::PointCloud2::ConstPtr& pointcloud);
-        void insertPointcloudWithTfCamDown(const sensor_msgs::PointCloud2::ConstPtr& pointcloud);
-        void evasionCallback(const multiagent_collision_check::Segment& segmentMsg);
+    void posCallback(const geometry_msgs::PoseWithCovarianceStamped& pose);
+    void odomCallback(const nav_msgs::Odometry& pose);
+    bool plannerCallback(nbvplanner::nbvp_srv::Request& req, nbvplanner::nbvp_srv::Response& res);
+    void insertPointcloudWithTf(const sensor_msgs::PointCloud2::ConstPtr& pointcloud);
+    void insertPointcloudWithTfCamUp(const sensor_msgs::PointCloud2::ConstPtr& pointcloud);
+    void insertPointcloudWithTfCamDown(const sensor_msgs::PointCloud2::ConstPtr& pointcloud);
+    void evasionCallback(const multiagent_collision_check::Segment& segmentMsg);
 
-    };
+};
 }
 
 #endif // NBVP_H_
